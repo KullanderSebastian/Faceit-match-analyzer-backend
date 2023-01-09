7ef4dbcd30e702fdbb0ed077cc0e9b2b02c5c056
KullanderSebastian <77612881+KullanderSebastian@users.noreply.github.com>
2023-01-09 16:38:59 +0100

    Delete calcProbabilityWin.js

--- calcProbabilityWin.js
+++
@@ -1,8 +0,0 @@
-const WIN_WEIGHT = 3;
-const LOOSE_WEIGHT = 1;
-const KR_WEIGHT = 4;
-
-const TEAMKRWEIGHT = 500;
-const TEAMELOWEIGHT  = 500;
-
-const latestMatches = await this.faceit.GetLatestMatches(user.id, (depth)? 2000 : 50 : 0);
